5f83e2594fe88f6c0e4a37d1fe812cc7835c9a37
Régis Martiny <regismartiny@gmail.com>
2023-07-21 18:23:50 -0300

    Corrige erro refresh token

--- volumes/lembrador-contas/src/util/googleAuth.js
+++ volumes/lembrador-contas/src/util/googleAuth.js
@@ -24,25 +24,13 @@ async function authenticate() {
     oAuth2Client.on('tokens', (tokens) => {
         console.log('tokens event')
         if (tokens.refresh_token) {
-            // store the refresh_token in my database!
             console.log(tokens.refresh_token);
             storeTokens(tokens)
         }
         console.log(tokens.access_token);
     });
-    
-    // Make a simple request to the People API using our pre-authenticated client. The `request()` method
-    // takes an GaxiosOptions object.  Visit https://github.com/JustinBeckwith/gaxios.
-    const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
-    const res = await oAuth2Client.request({url});
-    console.log(res.data);
 
-    // After acquiring an access_token, you may want to check on the audience, expiration,
-    // or original scopes requested.  You can do that with the `getTokenInfo` method.
-    const tokenInfo = await oAuth2Client.getTokenInfo(
-        oAuth2Client.credentials.access_token
-    );
-    console.log(tokenInfo);
+    return oAuth2Client;
 }
 
 /**
@@ -78,42 +66,41 @@ function getNewToken(oAuth2Client) {
         // Generate the url that will be used for the consent dialog.
         const authorizeUrl = oAuth2Client.generateAuthUrl({
             access_type: 'offline',
+            prompt: 'consent',
             scope: SCOPES,
         });
     
         // Open an http server to accept the oauth callback. In this simple example, the
         // only request to our webserver is to /oauth2callback?code=<code>
-        const server = http
-            .createServer(async (req, res) => {
+        const server = http.createServer(async (req, res) => {
             try {
                 if (req.url.indexOf('/oauth2callback') > -1) {
-                // acquire the code from the querystring, and close the web server.
-                const qs = new url.URL(req.url, 'http://localhost:3000')
-                    .searchParams;
-                const code = qs.get('code');
-                console.log(`Code is ${code}`);
-                res.end('Authentication successful! Please return to the console.');
-                server.destroy();
-    
-                // Now that we have the code, use that to acquire tokens.
-                const r = await oAuth2Client.getToken(code);
-                // Make sure to set the credentials on the OAuth2 client.
-                oAuth2Client.setCredentials(r.tokens);
-                // Store the token to disk for later program executions
-                storeTokens(r.tokens)
-                console.info('Tokens acquired.');
-                resolve(oAuth2Client);
+                    // acquire the code from the querystring, and close the web server.
+                    const qs = new url.URL(req.url, 'http://localhost:3000')
+                        .searchParams;
+                    const code = qs.get('code');
+                    console.log(`Code is ${code}`);
+                    res.end('Authentication successful! Please return to the console.');
+                    server.destroy();
+        
+                    // Now that we have the code, use that to acquire tokens.
+                    const r = await oAuth2Client.getToken(code);
+                    // Make sure to set the credentials on the OAuth2 client.
+                    oAuth2Client.setCredentials(r.tokens);
+                    // Store the token to disk for later program executions
+                    storeTokens(r.tokens)
+                    console.info('Tokens acquired.');
+                    resolve(oAuth2Client);
                 }
             } catch (e) {
                 reject(e);
             }
-            })
-            .listen(3000, () => {
-                console.log(authorizeUrl)
-                //TODO: Reativar. Antivirus está bloqueando
-                // open the browser to the authorize url to start the workflow
-                // open(authorizeUrl, {wait: false}).then(cp => cp.unref());
-            });
+        }).listen(3000, () => {
+            console.log(authorizeUrl)
+            //TODO: Reativar. Antivirus está bloqueando
+            // open the browser to the authorize url to start the workflow
+            // open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+        });
         destroyer(server);
     })
 }
