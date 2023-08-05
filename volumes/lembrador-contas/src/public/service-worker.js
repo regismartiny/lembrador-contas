9845045d21c15437e8b0c830ed57d6c52c4a9db1
Régis Martiny <regismartiny@gmail.com>
2023-08-05 19:56:09 -0300

    Fix notifications

--- volumes/lembrador-contas/src/public/service-worker.js
+++ volumes/lembrador-contas/src/public/service-worker.js
@@ -1,34 +1,39 @@
-navigator.serviceWorker.register('/service-worker.js')
-   .then(async serviceWorker => {
-      console.log('Service worker registered')
+// Check if the service worker API is supported
+if ("serviceWorker" in navigator) {
+   navigator.serviceWorker.register('/service-worker.js')
+      .then(async serviceWorker => {
+         console.log('Service worker registered.')
 
-      let subscription = await serviceWorker.pushManager.getSubscription()
+         let subscription = await serviceWorker.pushManager.getSubscription()
 
-      if (!subscription) {
-         const publicKeyResponse = await send('GET', '/notifications/push/public_key')
+         if (!subscription) {
+            const publicKeyResponse = await send('GET', '/notifications/push/public_key')
 
-         subscription = await serviceWorker.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: publicKeyResponse.data.publicKey
+            subscription = await serviceWorker.pushManager.subscribe({
+               userVisibleOnly: true,
+               applicationServerKey: JSON.parse(publicKeyResponse).publicKey
+            })
+         }
+
+         await send('POST', '/notifications/push/register', {
+            subscription
          })
-      }
 
-      await send('POST', '/notifications/push/register', {
-         subscription
+         //Teste de notificacao
+         await send('POST', '/notifications/push/send', subscription)
       })
 
-      //Teste de notificacao
-      await send('POST', '/notifications/push/send')
-   })
-
-function send(method, url, data) {
-   return new Promise(function (resolve, reject) {
-      const req = new XMLHttpRequest();
-      req.addEventListener("load", () => {
-         console.log(this.responseText)
-         resolve(this.responseText)
+   function send(method, url, data) {
+      return new Promise(function (resolve, reject) {
+         const req = new XMLHttpRequest();
+         req.onreadystatechange = function() {
+            if (this.readyState == 4 && (this.status >= 200 || this.status < 300)) {
+               resolve(this.responseText)
+            }
+         };
+         req.open(method, url)
+         req.setRequestHeader('Content-type', 'application/json');
+         req.send(JSON.stringify(data))
       });
-      req.open(method, url)
-      req.send(data)
-   });
+   }
 }
--- volumes/lembrador-contas/src/routes/notifications.js
+++ volumes/lembrador-contas/src/routes/notifications.js
@@ -2,38 +2,37 @@ const express = require('express')
 const router = express.Router()
 const WebPush = require('web-push')
 
-//console.log(WebPush.generateVAPIDKeys())
+const apiKeys = WebPush.generateVAPIDKeys()
 
-const publickKey = 'BESn_XPIJdTml9-Z7lmMr4YrwpCfGN1KEiS4q25UrzjU3mW3_ZX06cRsLgotfKtfkEM_Cvt2oTNbl9VRx7Amg3w'
-const privateKey = 'qPeMZfrOwGabPGki5WJyVlF_6gqonbdlaB2PCtmxBlg'
+const publicKey = apiKeys.publicKey
+const privateKey = apiKeys.privateKey
 
-WebPush.setVapidDetails('https://lembradordecontas.martiny.tech', publickKey, privateKey)
+WebPush.setVapidDetails('https://lembradordecontas.martiny.tech', publicKey, privateKey)
 
 
 router.get('/push/public_key', function (req, res) {
-   return res.status(200).json({publickKey}).send()
+   console.log("/push/public_key", req.body)
+   return res.status(200).json({publicKey}).send()
 });
 
 router.post('/push/register', function (req, res) {
-   console.log(req.body)
+   console.log("/push/register", req.body)
    return res.status(201).send()
 });
 
 router.post('/push/send', function (req, res) {
-   console.log(req.body)
-   const sub = req.body.subscription
+   console.log("/push/send", req.body)
+   const sub = req.body
 
    const sendPushBody = {
-      subscription: {
-         endpoint: sub.endpoint,
-         keys: {
-            p256dh: sub.keys.p256dh,
-            auth: sub.keys.auth
-         }
+      endpoint: sub.endpoint,
+      keys: {
+         p256dh: sub.keys.p256dh,
+         auth: sub.keys.auth
       }
    }
 
-   WebPush.sendNotification(sendPushBody, 'Teste de notificação')
+   WebPush.sendNotification(sendPushBody, 'Bem vindo ao Lembrador de Contas!')
 
    return res.status(201).send()
 });
