c6bd8d4c9ee5b2f3fc3271b87e468ed4f39abede
Régis Martiny <regismartiny@gmail.com>
2025-04-10 18:55:28 -0300

    Updates cpflEmailParser to work with new email template

--- volumes/lembrador-contas/src/parser/cpflEmailParser.js
+++ volumes/lembrador-contas/src/parser/cpflEmailParser.js
@@ -17,47 +17,47 @@ async function fetch(address, subject, period) {
         return []
     }
 
-    let parsedData = []
-
     let message = messages[0] //use only the first message
-    const info = await parse(message);
-    if (!info) {
+    const parsedData = await parseEmailData(message);
+    if (!parsedData) {
         return []
     }
-    console.log("info", info);
+    console.log("parsedData", parsedData);
     
-    parsedData.push ({ 
-        dueDate: info.vencimento, 
-        value: info.valor 
-    })
+    let response = { 
+        dueDate: parsedData.vencimento, 
+        value: parsedData.valor 
+    }
 
-    return parsedData
+    return [response]
 }
 
-function parse(msg) {
+function parseEmailData(msg) {
     try {
-        console.log("parse()")
-        let data = msg.payload.body.data;
+        console.log("parseEmailData()")
+        let data = msg.payload.body.data
         if (!data) return
         
-        let base64str = base64Util.fixBase64(data);
-        var text = base64Util.base64ToText(base64str);
-        const dom = new jsdom.JSDOM(text);
-        const elemValor = querySelectorIncludesText(dom, 'b', 'R$')
-        const elemMesReferencia = elemValor.previousElementSibling.previousElementSibling
-        const elemVencimento = elemMesReferencia.previousElementSibling.previousElementSibling
-        const elemInstalacao = elemVencimento.previousElementSibling.previousElementSibling
-        const valor = elemValor.innerHTML
-        const mesReferencia = elemMesReferencia.innerHTML
-        const vencimento = elemVencimento.innerHTML
-        const instalacao = elemInstalacao.innerHTML
+        let base64str = base64Util.fixBase64(data)
+        var text = base64Util.base64ToText(base64str)
+        
+        const dom = new jsdom.JSDOM(text)
+
+        const elemValor = querySelectorIncludesText(dom, 'span', 'R$')
+        const elemVencimento = elemValor.parentElement.parentElement.previousElementSibling.childNodes[1].childNodes[1]
+        const elemMesReferencia = elemValor.parentElement.parentElement.previousElementSibling.previousElementSibling.childNodes[1].childNodes[1]
+        const elemInstalacao = elemValor.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.childNodes[1].childNodes[1]
+        const valor = elemValor.innerHTML.substring(4)
+        const vencimento = elemVencimento.innerHTML.substring(4)
+        const mesReferencia = elemMesReferencia.innerHTML.substring(4)
+        const instalacao = elemInstalacao.innerHTML.substring(4)
         return { instalacao, 
             vencimento : moment(vencimento, "DD/MM/YYYY").toDate(), 
             mesReferencia, 
             valor : Number.parseFloat(valor.replace(",",".").substring(3, valor.length-1)) 
         }
     } catch(e) {
-        console.error("Failed to get info from HTML email", e);
+        console.error("Failed to get info from HTML email", e)
     }
 }
 
